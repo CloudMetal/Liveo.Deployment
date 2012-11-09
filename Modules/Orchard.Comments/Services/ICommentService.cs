3ea052656599e40d9170742109a7f1d261e2d165
GQAdonis <gqadonis@me.com>
2012-11-09 04:49:32 -0600

    orchard update and latest code

--- Modules/Orchard.Comments/Services/ICommentService.cs
+++ Modules/Orchard.Comments/Services/ICommentService.cs
@@ -10,11 +10,8 @@ namespace Orchard.Comments.Services {
         CommentPart GetComment(int id);
         ContentItemMetadata GetDisplayForCommentedContent(int id);
         ContentItem GetCommentedContent(int id);
-        CommentPart CreateComment(CreateCommentContext commentRecord, bool moderateComments);
-        void UpdateComment(int id, string name, string email, string siteName, string commentText, CommentStatus status);
         void ApproveComment(int commentId);
         void UnapproveComment(int commentId);
-        void MarkCommentAsSpam(int commentId);
         void DeleteComment(int commentId);
         bool CommentsDisabledForCommentedContent(int id);
         void DisableCommentsForCommentedContent(int id);
--- Modules/Orchard.Comments/Tokens/CommentTokens.cs
+++ Modules/Orchard.Comments/Tokens/CommentTokens.cs
@@ -21,25 +21,29 @@ namespace Orchard.Comments.Tokens {
         public Localizer T { get; set; }
 
         public void Describe(dynamic context) {
-            context.For("Content", T("Content Items"), T("Content Items"))
+            context.For("Content", T("Comments"), T("Comments"))
                 .Token("CommentedOn", T("Commented On"), T("The content item this comment was created on."))
                 .Token("CommentMessage", T("Comment Message"), T("The text of the comment itself"))
                 .Token("CommentAuthor", T("Comment Author"), T("The author of the comment."))
+                .Token("CommentAuthorUrl", T("Comment Author Url"), T("The url provided by the author of the comment."))
+                .Token("CommentAuthorEmail", T("Comment Author Email"), T("The email provided by the author of the comment."))
                 ;
         }
 
         public void Evaluate(dynamic context) {
             context.For<IContent>("Content")
-                .Token("CommentedOn", (Func<IContent, object>)(content => content.As<CommentPart>().Record.CommentedOn))
-                .Chain("CommentedOn", "Content", (Func<IContent, object>)(content => _contentManager.Get(content.As<CommentPart>().Record.CommentedOn)))
-                .Token("CommentMessage", (Func<IContent, object>)(content => content.As<CommentPart>().Record.CommentText))
+                .Token("CommentedOn", (Func<IContent, object>)(content => content.As<CommentPart>().CommentedOn))
+                .Chain("CommentedOn", "Content", (Func<IContent, object>)(content => _contentManager.Get(content.As<CommentPart>().CommentedOn)))
+                .Token("CommentMessage", (Func<IContent, object>)(content => content.As<CommentPart>().CommentText))
                 .Token("CommentAuthor", (Func<IContent, object>)CommentAuthor)
+                .Token("CommentAuthorUrl", (Func<IContent, object>)(content => content.As<CommentPart>().SiteName))
+                .Token("CommentAuthorEmail", (Func<IContent, object>)(content => content.As<CommentPart>().Email))
                 ;
         }
 
         private static string CommentAuthor(IContent comment) {
             var commentPart = comment.As<CommentPart>();
-            return String.IsNullOrWhiteSpace(commentPart.Record.UserName) ? commentPart.Record.Author : commentPart.Record.UserName;
+            return String.IsNullOrWhiteSpace(commentPart.UserName) ? commentPart.Author : commentPart.UserName;
         }
     }
 }
